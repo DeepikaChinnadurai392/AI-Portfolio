5d25d85a3d418518af2f2ccbfa58d3391e69ebe7
Deepika Chinnadurai <deepika@gmail.com>
2026-07-15 19:26:16 +0530

    Use environment variables for database

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,15 @@
+import os
 import mysql.connector
 
 
 def get_db_connection():
 
     db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="navee@2025_DB",
-        database="ai_portfolio"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
 
     return db
